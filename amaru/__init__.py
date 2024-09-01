6a9a16e60c6648927e43a6cad11f61804f275a32
Sergio Ramirez <sergio.ramrez@uc.cl>
2024-09-01 19:43:23 -0400

    feat: add rich module import in init, add author, version and email

--- amaru/__init__.py
+++ amaru/__init__.py
@@ -1,1 +1,13 @@
 from .goes import Goes
+
+
+__author__ = "Sergio Ramírez"
+__email__ = "sergio.ramrez@uc.cl"
+__version__ = 1.2
+
+try:
+    import rich
+
+except ImportError:
+    print("rich no esta instalado")
+    print("instalar con pip install rich")

d07344e402581b4f873a929baa54728c9d2377aa
Sergio Ramirez <sergio.ramrez@uc.cl>
2025-09-12 11:56:47 -0300

    refractor: move Goes datelist to __init__

--- src/amaru/goes.py
+++ src/amaru/goes.py
@@ -1,9 +1,11 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from pathlib import Path
 import requests
 from rich.console import Console
 import os
+from reprlib import recursive_repr
 from ._goes_r import _GoesAWS
+from ._goes_noaa import _GoesNOAA
 import warnings
 from .datetools import timerange
 from typing import Generator
@@ -15,7 +17,7 @@ class Goes:
     Class for Interacting with the download process, using date and the metadata.
 
     Depending of the dates provided, it will download Netcdf files from either NOAAH or
-    AWS. The satellites used can be also specified.
+    AWS. The satellites products used can be also specified.
 
     Parameters
     ----------
@@ -30,17 +32,23 @@ class Goes:
         self,
         inicio: datetime,
         fin: datetime,
-        product: str,
+        product: str=None,
+        satellite: list[str] | dict[str, str] = None,
         path: str | None = Path().home() / "goes_downloaded",
-        cpu=os.cpu_count()
+        cpu=os.cpu_count(),
+        skip_invalid_data: bool = False
     ):
         self._inicio = inicio
         self._fin = fin
         self._path = Path(path)
         self._console = Console()
-        self._datelist = None
+        if inicio == fin:
+            raise RuntimeError("No Data range, select different datetimes")
+        self._datelist = timerange(self._inicio, self._fin)
         self._cpu = cpu
-        self.product = product
+        self._product = product
+        self.satellite = satellite
+        self.skip = skip_invalid_data
 
     # Hacer que la clase solo sea de lectura
 
@@ -68,13 +76,17 @@ class Goes:
     def datelist(self):
         """tupla de fechas, incluye horas del inicio y del fin,
         si es que el objeto es datetime"""
-        return self._time_range()
+        return self._datelist
 
     @property
     def cpu(self):
         """Numero de cpus que se utilizaran para la descarga.
         Por defecto es el numero de cpus disponibles en el equipo"""
         return self._cpu
+
+    @property
+    def product(self):
+        return self._product
 
     def check_noaa(self):
         """checks connection to goes noaa"""
@@ -98,28 +110,27 @@ class Goes:
         except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError):
             warnings.warn("Conection not available to AWS")
 
-    def _time_range(self) -> Generator:
-        if self.inicio == self.fin:  # si son la misma fecha
-            raise RuntimeError("No hay rango de tiempo, seleccionar fechas distintas")
-
-        return timerange(self.inicio, self.fin)
-
     def descargar(self) -> None:
 
         if self.inicio < CHANGE_TO_AWS and self.fin < CHANGE_TO_AWS:
             # Corresponde a las imagenes del sitio de noaah
-            pass
+            noaah = _GoesNOAA(
+                self.datelist, self.path, self.console, self.cpu, self.satellite, self.skip
+            )
+            noaah._prepare_download()
+            del noaah
 
         elif CHANGE_TO_AWS <= self.inicio and CHANGE_TO_AWS < self.fin:
             # Imagenes de AWS
-            aws = _GoesAWS(self.datelist, self.path, self.console, self.cpu, self.product)
-            aws._prepared_download()
+            aws = _GoesAWS(
+                self.datelist, self.path, self.console, self.cpu, self.product
+            )
+            aws._prepare_download()
             del aws
 
         elif self.inicio < CHANGE_TO_AWS and CHANGE_TO_AWS <= self.fin:
             "return timerange(self.inicio, CHANGE_TO_AWS + timedelta(hours=-1))"
-            "timerange(CHANGE_TO_AWS, self.fin)"
-            pass
+            "timerange(CHANGE_TO_AWS, self.fin)"     
 
     def __log(self, string):
         self.console.print(string)
@@ -133,35 +144,10 @@ class Goes:
             f"  CPUs: {self.cpu}\n"
         )
 
-    def __repr__(self) -> str:
-        return f"""
-        <div style="font-family: sans-serif;">
-            <h4 style="margin-bottom: 0.5em;">Goes </h4>
-            <table style="
-                border-collapse: collapse;
-                text-align: left;
-                font-size: 14px;
-                ">
-                <tr>
-                    <th style="padding: 4px 8px; border-bottom: 1px solid #ccc;">Attribute</th>
-                    <th style="padding: 4px 8px; border-bottom: 1px solid #ccc;">Value</th>
-                </tr>
-                <tr>
-                    <td style="padding: 4px 8px;">Dates</td>
-                    <td style="padding: 4px 8px;">{self.inicio} : {self.fin}</td>
-                </tr>
-                <tr>
-                    <td style="padding: 4px 8px;">Path</td>
-                    <td style="padding: 4px 8px;">{self.path}</td>
-                </tr>
-                <tr>
-                    <td style="padding: 4px 8px;">Datelist</td>
-                    <td style="padding: 4px 8px;">{self.datelist}</td>
-                </tr>
-                <tr>
-                    <td style="padding: 4px 8px;">CPUs</td>
-                    <td style="padding: 4px 8px;">{self.cpu}</td>
-                </tr>
-            </table>
-        </div>
-        """
+    #    def forward_traking
+
+    @recursive_repr()
+    def _repr_html_(self) -> str:
+
+        html = [f"Goes class: hours: {len(list(self.datelist))}"]
+        return "\n".join(html)
